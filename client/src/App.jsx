cc3e88e685eeb940cbace922595ab9de6792ed0a
Pablo Lara <pablolara01@yahoo.com>
2023-11-28 20:03:59 -0500

    Changes to featred, footer, and app jsx files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,22 @@
-import './App.css'
-import Navbar from './components/Navbar.jsx'
-// import Carousel from './components/Carousel.jsx'
-// import Featured from './components/Featured.jsx'
-import NoMatch from './components/NoMatch';
-// import Footer from './components/Footer.jsx'
-import {Route, Routes } from 'react-router-dom';
+import "./App.css"
+import Navbar from "./components/Navbar.jsx"
+import Carousel from "./components/Carousel.jsx"
+import Featured from "./components/Featured.jsx"
+import NoMatch from "./components/NoMatch";
+import Footer from "./components/Footer.jsx"
+import {Route, Routes } from "react-router-dom";
 
 
 export default function App() {
   return (
-    <div className='gamerrank-page'>
+    <div className="gamerrank-page">
       <Routes>
         <Route path="/" element={<Navbar/>}/>
         <Route path="*" element={<NoMatch/>}/>
       </Routes>
-      {/* <Carousel />
+      <Carousel />
       <Featured />
-      <Footer /> */}
+      <Footer />
     </div>
   )
 }
