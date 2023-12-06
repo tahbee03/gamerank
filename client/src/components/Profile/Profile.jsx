5ca9ce7daac75e01d21403a42c2598ce487b1064
Sri Tarun <sritarung96@gmail.com>
2023-12-05 19:09:11 -0500

    bug fixes in profile

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -21,7 +21,7 @@ function Profile() {
                 <div className="top">
                     <div className="profile">
                         <img src="default-avatar.jpg" alt="Avatar"></img>
-                        <div className="name">Name</div>
+                        <div className="name">{JSON.parse(user).username}</div>
                         <div className="edit-btn"><button>Edit Profile</button></div>
                     </div>
                     <div className="bar">
