7efd09176a506325d9f99f74d0e2705d2592413d
Fu Jun Pan <fujunp9@gmail.com>
2024-05-01 04:47:22 -0400

    usernames now are unique

--- server/models/user_model.js
+++ server/models/user_model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {type: String, required: true},
+    username: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     pfp: {type: String},
