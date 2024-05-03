3aa06fba38cb62974b8213bec4df529024ff8404
Pablo Lara <pablolara01@yahoo.com>
2024-05-03 17:38:34 -0400

    Updated some css names to avoid bugs.

--- client/src/components/FeaturedGame/FeaturedGame.jsx
+++ client/src/components/FeaturedGame/FeaturedGame.jsx
@@ -3,10 +3,10 @@ import "./FeaturedGame.css";
 export default function FeaturedGame({ title, picture, reviews }) {
     return (
         <div className="featured-game">
-            <img className="game-pic" src={picture} alt="picture-image" />
+            <img className="featured-game-pic" src={picture} alt="picture-image" />
             <div>
                 <hr />
-                <p className="game-title">{title}</p>
+                <p className="featured-game-title">{title}</p>
                 <p>{reviews} reviews</p>
             </div>
         </div>
--- client/src/components/GamePage/GamePage.jsx
+++ client/src/components/GamePage/GamePage.jsx
@@ -59,7 +59,6 @@ export default function GamePage() {
                     </Row>
                 </Container>
             )}
-            <Footer />
         </>
     );
 }
