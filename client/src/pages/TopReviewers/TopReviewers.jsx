abb3ed515a632c58161dd2b9651b2cb18f72a600
Pablo Lara <pablolara01@yahoo.com>
2024-05-29 17:54:47 -0400

    Updated TopReviewers Stylization, including format of the list and title, and added a hover effect to highlight which user your mouse is on.

--- client/src/pages/TopReviewers/TopReviewers.jsx
+++ client/src/pages/TopReviewers/TopReviewers.jsx
@@ -2,6 +2,9 @@ import './TopReviewers.css';
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"; // useNavigate()
 import Navbar from "../../components/Navbar/Navbar.jsx";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 const server = import.meta.env.VITE_BACKEND_SERVER;
 
 export default function TopReviewers() {
@@ -25,14 +28,20 @@ export default function TopReviewers() {
   return (
     <>
       <Navbar />
-      <div>
-        <h1>Top Reviewers by Reviews</h1>
-        {topReviewers.map((reviewer,) => (
-          <div key={reviewer._id}>
-            User [<span className="user" onClick={() => navigate(`/Profile/${reviewer.username}`)}>{reviewer.username}</span>] with {reviewer.reviewCount} reviews
-          </div>
-        ))}
-      </div>
+        <Container style={{ maxWidth: '800px', margin: '0 auto', padding: '20px' }} className='top-reviewers-container'>
+          <Row>
+            <Col>
+              <div className='top-reviewers-title'>
+                <h1>Top Reviewers by Reviews</h1>
+              </div>
+                {topReviewers.map((reviewer,) => (
+                  <div className='top-reviewers-list' key={reviewer._id}>
+                    User [<span className="user" onClick={() => navigate(`/Profile/${reviewer.username}`)}>{reviewer.username}</span>] with {reviewer.reviewCount} reviews
+                  </div>
+                ))}
+            </Col>
+          </Row>
+        </Container>
     </>
   );
 }
